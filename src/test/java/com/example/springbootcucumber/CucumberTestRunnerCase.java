a63950c7feaa29021ae157501c84c369964cf9a3
Saleemfero1 <Saleemfero8143@gmail.com>
2023-11-23 12:00:20 +0530

    updated report location

--- src/test/java/com/example/springbootcucumber/CucumberTestRunnerCase.java
+++ src/test/java/com/example/springbootcucumber/CucumberTestRunnerCase.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/features"},
-        plugin = {"pretty", "json:target/cucumber-report.json", "html:target/cucumber-report.html"},
+        plugin = {"pretty", "json:target/cucumber-report.json", "html:test-output/html-report/cucumber-report.html"},
         monochrome = true,
         glue = {"com.example.springbootcucumber.cucumberglue"})
 public class CucumberTestRunnerCase {
